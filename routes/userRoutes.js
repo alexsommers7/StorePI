14dffec9d25260cad2d7f037ea8673beaea81498
alexsommers7 <asommers22@hotmail.com>
2022-05-14 17:27:35 -0700

    remove password routes and methods

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,8 +10,6 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
-router.post('/forgot-password', authController.forgotPassword);
-router.patch('/reset-password/:token', authController.resetPassword);
 
 router
   .route('/current')

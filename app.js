e2d57d2b22f2785cf08f2308d73f333936781cba
alexsommers7 <asommers22@hotmail.com>
2022-05-14 18:13:53 -0700

    increase rate limit

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const limiter = rateLimit({
-  max: 100,
+  max: 300,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP. Please try again in an hour.',
 });
